1192b3393aefaab0fc6506941b68f1d1bd6272e7
Dhiraj Jagtap <djagtap@qualys.com>
2025-11-09 02:57:01 +0530

    Fixes and Ui changes

--- src/components/orbit/Header.tsx
+++ src/components/orbit/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
       <div className="flex items-center gap-3 mb-2">
         {/* <Orbit className="h-10 w-10 text-primary" /> */}
         <img src="/qualys_logo_1.svg" alt="Qualys" className="h-12 w-12 text-primary" />
-        <h1 className="text-4xl font-bold tracking-tight bg-gradient-to-r from-primary to-primary/60 bg-clip-text text-transparent">
+        <h1 className="text-4xl font-bold tracking-[0.1em] bg-gradient-to-r from-primary to-primary/60 bg-clip-text text-transparent">
           ORBIT
         </h1>
       </div>
